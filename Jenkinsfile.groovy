45e8c07f2869fa394520363b59e9dd95c30cecbf
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-28 14:31:11 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ node
 {
   stage("Automate SCDF")
   {
-      bat '''copy C:\\Apps\\Jenkins\\jobs\\Ui-based-maven-app\\workspace\\target\\*.war C:\\Apps\\Jenkins\\workflow-libs'''
+      bat '''copy C:\\Programs\\*.jar C:\\Apps\\Jenkins\\workflow-libs'''
       // bat ''' cd C:\\Apps\\Jenkins\\workflow-libs && java -jar spring-cloud-dataflow-shell-2.0.1.RELEASE.jar'''
   }
 }
