5013d0c4779c7e19c9a6bb6b68840c2e7550b289
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-29 14:25:26 +0530

    Create Jenkins_undeploy.groovy

---
+++ Jenkins_undeploy.groovy
@@ -0,0 +1,8 @@
+node
+{
+  stage("Automate SCDF")
+  {
+      bat '''copy C:\\Programs\\*.jar C:\\Apps\\Jenkins\\workflow-libs'''
+       bat ''' cd C:\\Apps\\Jenkins\\workflow-libs && java -jar spring-cloud-dataflow-shell-2.0.1.RELEASE.jar --spring.shell.commandFile=spring-undeploy.shell'''
+  }
+}
