409439012e4d57a8c940f2312126453d1e27c4c2
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-30 11:38:29 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,13 +3,7 @@ node
   stage("Automate SCDF")
   {
       bat '''copy C:\\Programs\\*.jar C:\\Apps\\Jenkins\\workflow-libs'''
-    checkout([$class: 'GitSCM',
-			  branches: [[name: '*/master']],
-			  userRemoteConfigs: [
-				[url: 'https://github.com/SujataKale97/scdf.git',
-				 credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c']
-			  ]
-			])  
+    
        bat ''' cd C:\\Apps\\Jenkins\\workflow-libs && java -jar spring-cloud-dataflow-shell-2.0.1.RELEASE.jar --spring.shell.commandFile=spring.shell'''
   }
 }
